310e456aa1a3db92cbf49539ca567d92ee5dcb3f
Jaime Hernández <131257598+jaimeehh@users.noreply.github.com>
2026-09-20 11:43:29 +0200

    Cache interview packs module

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@ const APP_SHELL = [
   '/story-profiles.js',
   '/custom-questions.js',
   '/app.js',
-  '/company-motivations.js',
+  '/company-motivations.js',\n  '/interview-packs.js',
   '/manifest.webmanifest',
   '/icons/icon-192.png',
   '/icons/icon-512.png'
